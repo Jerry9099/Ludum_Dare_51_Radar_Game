a9e87c31f8a773d599a7bc8c01cd791fe997e1dc
david tuvshin <davidtuvshin10@g.ucla.edu>
2022-10-01 15:58:14 -0700

    RadarDisplay
    uhh. I think I may have done this wrong. But here should be the scene with the radar displays with viewports :)

---
+++ MeshInstance2.gd
@@ -0,0 +1,17 @@
+extends MeshInstance
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	#rotation_degrees = Vector3(0,lerp(rotation_degrees.y,target_rotation.y,speed*delta),0)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
