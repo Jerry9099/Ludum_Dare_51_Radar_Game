dde10c91380adcae3df717985cc2fcd5b2b1a8fa
Jerry9099 <93056988+Jerry9099@users.noreply.github.com>
2022-10-01 22:44:11 -0700

    Camera Limits + Map size Increase

--- Screen_Enemies/Player.gd
+++ Screen_Enemies/Player.gd
@@ -5,6 +5,13 @@ signal hit
 export var speed = 4
 export var fall_acceleration = 75
 
+export var limit_size_square = 30
+
+var top_limit = limit_size_square
+var bottom_limit = -limit_size_square
+var right_limit = limit_size_square
+var left_limit = -limit_size_square
+
 var velocity = Vector3.ZERO
 var HP
 
@@ -16,17 +23,18 @@ func _ready():
 func _physics_process(delta):
 	# Local variable to store the input direction.
 	var direction = Vector3.ZERO
-	
+	print(global_translation.x)
+	print(global_translation.z)
 	# Check for each move input and update the direction
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("move_right") && global_translation.x < right_limit:
 		direction.x += 1
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("move_left") && global_translation.x > left_limit:
 		direction.x -= 1
-	if Input.is_action_pressed("move_back"):
+	if Input.is_action_pressed("move_back") && global_translation.z < top_limit:
 # Notice how we are working with the vector's x and z axes.
 		# In 3D, the XZ plane is the ground plane.
 		direction.z += 1
-	if Input.is_action_pressed("move_forward"):
+	if Input.is_action_pressed("move_forward") && global_translation.z > bottom_limit:
 		direction.z -= 1
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
