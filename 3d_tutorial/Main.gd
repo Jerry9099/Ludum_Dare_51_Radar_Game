e05d38cddf1940a64d95ad36fd9917f5ed6ac57b
yutianrun7 <72716648+yutianrun7@users.noreply.github.com>
2022-10-01 19:44:53 -0700

    Bullet_hell_prototype

--- 3d_tutorial/Main.gd
+++ 3d_tutorial/Main.gd
@@ -1,26 +1,26 @@
 extends Node
 
-export (PackedScene) var mob_scene
+export (PackedScene) var missile_scene
 
 
 func _ready():
 	randomize()
 
 
-func _on_MobTimer_timeout():
-	# Create a new instance of the Mob scene.
-	var mob = mob_scene.instance()
+func _on_MissileTimer_timeout():
+	# Create a new instance of the Missile scene.
+	var missile = missile_scene.instance()
 	
 	# Choose a random location on the SpawnPath.
 	# We store the reference to the SpawnLocation node.
-	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
+	var missile_spawn_location = get_node("SpawnPath/SpawnLocation")
 	# And give it a random offset.
-	mob_spawn_location.unit_offset = randf()
+	missile_spawn_location.unit_offset = randf()
 	
 	var player_position = $Player.transform.origin
-	mob.initialize(mob_spawn_location.translation, player_position)
+	missile.initialize(missile_spawn_location.translation, player_position, $Player)
 	
-	add_child(mob)
+	add_child(missile)
 
 func _on_Player_hit():
-	$MobTimer.stop()
+	$MissileTimer.stop()
