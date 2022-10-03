aae677c43a3aa8ccc7355d64156545fc4a760ed3
JohnnyDz0707 <114839725+JohnnyDz0707@users.noreply.github.com>
2022-10-03 04:18:46 -0700

    Added Sounds, Added WASD Text, Added Button hover / click sound effects

---
+++ ButtonClickAudio.gd
@@ -0,0 +1,30 @@
+extends AudioStreamPlayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_START_button_down():
+	play() # Replace with function body.
+
+func _on_QUIT_button_down():
+	play() # Replace with function body.
+	
+func _on_pQUIT_button_down():
+	if get_tree().paused:
+		play()
+
+func _on_Button_button_down():
+	play() # Replace with function body.
+	
+func _on_RESUME_button_down():
+	if get_tree().paused:
+		play()
